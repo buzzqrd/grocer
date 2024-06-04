22d0523a83d7ac4c7ae2c6d9a9b09b4b5f1061be
buzzqrd <buzzqrd@protonmail.com>
2024-06-04 11:19:25 -0700

    added important functions

--- README.md
+++ README.md
@@ -19,3 +19,59 @@ fn main() {
 	println!("Scanned barcode: {}", upc_code);
 }
 ```
+
+### Why is it taking so long?
+
+Scans can take a while mostly because the barcode can be anywhere in the image and needs 
+to be found first. The whole image is scanned and every white section is checked to see if 
+it is the leading quiet zone on the left side of a barcode. 
+
+**To fix it**, for now you can compile with --release and it should go much faster.
+
+I'm working on adding features soon that allow the user to cut down on reliability for 
+speed if the application calls for it. For instance, if you know the barcode is always 
+in the center of the image, changing the settings would allow for very fast scanning 
+because it's easier to look for the barcode.
+
+
+## Help
+
+Help me out! If you have images of barcodes that won't scan with this library, let me know!
+Please send me any images that don't scan (buzzqrd@protonmail.com). I want to make sure that 
+this library is able to work for all barcodes within reason, and it will really help to 
+test it on a wide variety of barcodes. I've been taking lots of pictures of barcodes and 
+testing them with grocer. I've mostly had complete success, but I had some issues with a green 
+barcode that I found, so im working on making the black-white boundary auto-adjust to 
+capture a wider range of color-printed barcodes.
+
+If you want to send a barcode to me, make sure it's a supported format (UPC-A for now) and 
+email me at buzzqrd@protonmail.com.
+
+Thank you!
+
+## Future / In-Progress
+
+- Proper errors
+- Making scans that adjust the grayscale profile during scanning to scan barcodes made with different colors
+- Wider range of features and customization to be able to disable those features
+- Support for live video streams to scan barcodes from a computer-attatched camera
+- Support for a wider range of linear barcodes (UPC-E, FNSKU, EAN, Pharmacode, etc.)
+- Ability to read barcodes rotated vertically
+
+## Changelog
+
+0.1.0: Initial release
+	- Basic barcode scanning
+	- UPC-A support
+
+0.1.1: Updated docs
+	- Updates to some documentation
+
+0.1.2: Important features update
+	- More accurate scanning methods
+	- Structs for settings selection
+	- Return structs for barcode info
+	- Rejects any barcodes of the wrong size (UPC-A = 12 chars)
+
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,9 @@
 
 use std::env;
 use std::error::Error;
+use std::path::Path;
 use image::GenericImageView;
+
 
 
 #[derive(Debug)]
@@ -21,6 +23,19 @@ struct Bar {
 	modules: u32,
 	width: u32,
 }
+
+
+pub struct ScannerSettings {
+	pub high_speed: bool,
+}
+
+pub struct Barcode {
+	pub found: bool,
+	pub code: String,
+	pub orientation: u32, // not yet used
+	pub reversed: u32, // not yet used
+}
+
 
 fn pixel_bw(pixel: image::Rgba<u8>) -> u32 {
 	let thresh = 110; // Default: 110
@@ -275,26 +290,117 @@ fn scan_upc_line(file: &str, height_percent: f64) -> String {
 /// # Parameters
 /// 
 /// file: A string of the image filename to read from.
+/// settings: A ScannerSettings struct to select scanning options.
 ///
 /// # Return
 ///
-/// Returns a String of the 12-digit UPC code if the scan was successful. Returns an empty string if no barcode was found.
+/// Returns a Barcode struct representing any barcode scanned. Returns an empty string in the code field if no barcode was found
 ///
 /// # Examples
 /// 
 /// ```
-///	let upc_code = grocer::scan_upc("images/image.png");
+///     use grocer;
+///     let scan_settings = grocer::ScannerSettings { high_speed: false, };
+///	let barcode: grocer::Barcode = grocer::scan_upc("images/image.png", scan_settings);
+///	println!("Code: {}", barcode.code);
 /// ```
-pub fn scan_upc(file: &str) -> String {
+pub fn scan_upc(file: &str, settings: ScannerSettings) -> Barcode {
+	let mut barcode;
 	let mut code: String = String::from("");
+	
+	// Make a list of codes that are found
+	let mut codes: Vec<String> = Vec::new();
+	let mut code_counts: Vec<u32> = Vec::new();
+
+	if !Path::new(file).exists() {
+		return Barcode {
+			found: false,
+			code: code,
+			orientation: 0,
+			reversed: 0,
+		};
+	}
+
 
 	for percent in (0..100).step_by(5) {
 		code = scan_upc_line(file, (percent as f64) / 100.0);
+		
+		// Reject codes that are not the right size
+		if code.len() != 12 {
+			continue;
+		}
+
 		if code != "" {
-			break;
+			if settings.high_speed {
+				break;
+			}
+			else {
+				// Increment the code count or add it to the list
+				let mut found: bool = false;
+				for (i, c) in codes.iter().enumerate() {
+					if &code == c {
+						code_counts[i] += 1;
+						found = true;
+						break;
+					}
+				}
+				if !found {
+					codes.push(code.clone());
+					code_counts.push(1);
+				}
+			}
 		}
 	}
-	code
+
+	if settings.high_speed {
+		barcode = Barcode {
+			found: { 
+				if code == "" {
+					false
+				} else {
+					true
+				}
+			},
+			code: code,
+			orientation: 0,
+			reversed: 0,		
+		};
+	}
+	else {
+		
+		// Use the most frequently found code in the image
+		let mut max_found: u32 = 0;
+		let mut max_index: usize = 0;
+		for (i, &cnt) in code_counts.iter().enumerate() {
+			if cnt > max_found {
+				max_found = cnt;
+				max_index = i;
+			}
+		}
+
+		//println!("Code list: {:?}", codes);	
+		//println!("Code count: {:?}", code_counts);	
+	
+		code = codes[max_index].clone();
+		
+		barcode = Barcode {
+			found: { 
+				if code == "" {
+					false
+				} else {
+					true
+				}
+			},
+			code: code,
+			orientation: 0,
+			reversed: 0,		
+		};
+	}
+	barcode
 }
+
+
+
+
 
 
